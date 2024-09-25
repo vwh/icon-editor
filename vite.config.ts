70070f12a38e242120d944a550a78b1259570edb
vwh <vwhe@proton.me>
2024-09-25 18:39:32 +0300

    feat: add github pages support

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/icon-editor/",
   plugins: [
     compression(),
     react(),
