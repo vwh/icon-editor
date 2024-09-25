f553fa0c221b1592c670a15f31764c9c1ad344c5
vwh <vwhe@proton.me>
2024-09-25 18:53:59 +0300

    feat: added custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/icon-editor/",
   plugins: [
     compression(),
     react(),
