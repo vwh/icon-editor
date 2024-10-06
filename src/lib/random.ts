0cef9bcb753f63745abd9eaa86d7bbecc236f4e1
vwh <vwhe@proton.me>
2024-10-06 22:08:33 +0300

    fix: fixing random icon with no color

--- src/lib/random.ts
+++ src/lib/random.ts
@@ -10,7 +10,9 @@ export function randomIconName(): Icons {
 }
 
 export function randomIconColor(): string {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
 }
 
 export function randomGradient(): string {
